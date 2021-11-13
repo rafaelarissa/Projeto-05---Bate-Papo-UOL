0d424e7f163997413bb77f3bef2bc6121d609228
rafaelarissa <rafaela.larissalj@gmail.com>
2021-11-13 01:04:24 -0300

    rolagem automática por padrão

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function entrarNoChat() {
     const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name: nome});
 
     promessa.then(sucesso);
-    promessa.catch(trataErro);
+    promessa.catch(gerarMensagens);
 }
 entrarNoChat();
 
@@ -11,41 +11,44 @@ function sucesso() {
     const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
     
     promessa.then(gerarMensagens);
+    console.log("rui");
 }
 
-function trataErro(erro) {
-    const statusCode = erro.response.status;
-    console.log(statusCode);
-    	
-    prompt("Nome já em uso, digite um outro nome");
-
-}
-
-function gerarMensagens(resposta){
-    const dados = resposta.data;
-
-    const mensagens = document.querySelector(".mensagens");
-    mensagens.scrollIntoView();
+// function trataErro(erro) {
+    //     const statusCode = erro.response.status;
+    //     console.log(statusCode);
     
-    console.log('testarecarregar');
+    //     prompt("Nome já em uso, digite um outro nome");
     
-    for(i = 0; i < dados.length; i++) {
+    // }
+    
+    function gerarMensagens(resposta){
+        const dados = resposta.data;
         
-        if(dados[i].type === 'status') {
+        const mensagens = document.querySelector(".mensagens");
+        mensagens.innerHTML = ' ';
+
+        for(i = 0; i < dados.length; i++) {
+            
+            if(dados[i].type === 'status') {
             mensagens.innerHTML +=`
             <div class="status" data-identifier="message"><span class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> ${dados[i].text}</div>`
-        }
-        else if(dados[i].type === 'message'){
-            mensagens.innerHTML +=`
-            <div class="message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`            
-        }
-        else{
-            mensagens.innerHTML +=`
-            <div class="private-message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`            
-        }
-    }    
+            }
+            else if(dados[i].type === 'message'){
+                mensagens.innerHTML +=`
+                <div class="message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`            
+            }
+            else{
+                mensagens.innerHTML +=`
+                <div class="private-message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`            
+            }
+        } 
+        let lastmessage = document.querySelector(".mensagens div:last-child");
+        lastmessage.scrollIntoView();
+
 }
 
-// setInterval(sucesso, 3000);
-// clearInterval(setInterval);
+setInterval(sucesso, 3000); 
+
+
 
