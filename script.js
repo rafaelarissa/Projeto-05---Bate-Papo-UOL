2ff41b773ae0fdfc71bf339a4e4ad937e698ded7
rafaelarissa <rafaela.larissalj@gmail.com>
2021-11-12 23:44:46 -0300

    recarrega mensagens do servidor

--- script.js
+++ script.js
@@ -3,29 +3,34 @@ function entrarNoChat() {
     const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name: nome});
 
     promessa.then(sucesso);
-    promessa.catch(processarResposta);
+    promessa.catch(trataErro);
 }
 entrarNoChat();
 
 function sucesso() {
     const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
     
-    promessa.then(processarResposta);
+    promessa.then(gerarMensagens);
 }
 
+function trataErro(erro) {
+    const statusCode = erro.response.status;
+    console.log(statusCode);
+    	
+    prompt("Nome já em uso, digite um outro nome");
+
+}
 
 function gerarMensagens(resposta){
     const dados = resposta.data;
 
-    const mensagens = document.querySelector(".mensagens")
-
-    // const horario = document.querySelector(".horario").value;
-    // horario.innerHTML = dados.time;
-
-    console.log(dados);
-
-
+    const mensagens = document.querySelector(".mensagens");
+    mensagens.scrollIntoView();
+    
+    console.log('testarecarregar');
+    
     for(i = 0; i < dados.length; i++) {
+        
         if(dados[i].type === 'status') {
             mensagens.innerHTML +=`
             <div class="status" data-identifier="message"><span class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> ${dados[i].text}</div>`
@@ -38,16 +43,9 @@ function gerarMensagens(resposta){
             mensagens.innerHTML +=`
             <div class="private-message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`            
         }
-    }
-
-    //  if('tipo for igual a status'){
-    //     'mensagem recebe classe mensagem-status'
-    //  }else if('tipo for igual a private_message'){
-    //     ' a mensagem recebe classe mensagem-privada'
-    //  }else{
-    //     'mensagem recebe classe mensagem-normal'
-    //  }
-
-
-    // const nome = document.querySelector(".nome").value;    
+    }    
 }
+
+// setInterval(sucesso, 3000);
+// clearInterval(setInterval);
+
