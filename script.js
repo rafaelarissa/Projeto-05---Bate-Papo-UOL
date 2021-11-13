9668fd14b8a1f5b7e4e04ad8ca62aafc916e0657
rafaelarissa <rafaela.larissalj@gmail.com>
2021-11-13 17:54:14 -0300

    chat com mensagens reservadas

--- script.js
+++ script.js
@@ -1,54 +1,62 @@
+let nome = ' ';
+
 function entrarNoChat() {
-    const nome = prompt('Qual o seu nome?');
-    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name: nome});
+    nome = prompt('Qual o seu nome?');
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', { name: nome });
 
     promessa.then(sucesso);
-    promessa.catch(gerarMensagens);
+    promessa.catch(trataErro);
 }
 entrarNoChat();
 
 function sucesso() {
     const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
-    
+
     promessa.then(gerarMensagens);
-    console.log("rui");
 }
 
-// function trataErro(erro) {
-    //     const statusCode = erro.response.status;
-    //     console.log(statusCode);
-    
-    //     prompt("Nome já em uso, digite um outro nome");
-    
-    // }
-    
-    function gerarMensagens(resposta){
-        const dados = resposta.data;
-        
-        const mensagens = document.querySelector(".mensagens");
-        mensagens.innerHTML = ' ';
+function trataErro(erro) {
+    console.log(erro.response);
 
-        for(i = 0; i < dados.length; i++) {
-            
-            if(dados[i].type === 'status') {
-            mensagens.innerHTML +=`
-            <div class="status" data-identifier="message"><span class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> ${dados[i].text}</div>`
-            }
-            else if(dados[i].type === 'message'){
-                mensagens.innerHTML +=`
-                <div class="message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`            
-            }
-            else{
-                mensagens.innerHTML +=`
-                <div class="private-message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`            
-            }
-        } 
-        let lastmessage = document.querySelector(".mensagens div:last-child");
-        lastmessage.scrollIntoView();
+    if (erro.response.status === 400) {
+        alert("Nome já utilizado");
+        entrarNoChat();
+    }
 
 }
 
-setInterval(sucesso, 3000); 
+function gerarMensagens(resposta) {
+    const dados = resposta.data;
+    const mensagens = document.querySelector(".mensagens");
+    mensagens.innerHTML = ' ';
+
+    for (i = 0; i < dados.length; i++) {
+
+        if (dados[i].type === 'status') {
+            mensagens.innerHTML += `
+        <div class="status" data-identifier="message"><span class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> ${dados[i].text}</div>`
+        }
+        else if (dados[i].type === 'message') {
+            mensagens.innerHTML += `
+            <div class="message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> para <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`
+        }
+        else {
+            if (dados[i].to === nome) {
+                mensagens.innerHTML += `
+                <div class="private-message" data-identifier="message"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> reservadamente para <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`
+            }
+            else {
+                mensagens.innerHTML += `
+                <div class="private-message" data-identifier="message" style="display: none;"><span  class="horario">(${dados[i].time})</span> <span class="usuario">${dados[i].from}</span> reservadamente para <span class="usuario">${dados[i].to}</span> ${dados[i].text}</div>`
+            }
+        }
+    }
+    let lastmessage = document.querySelector(".mensagens div:last-child");
+    lastmessage.scrollIntoView();
+
+}
+
+setInterval(sucesso, 3000);
 
 
 
